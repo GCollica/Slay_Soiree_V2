c1b58602992d69195c38a26a1fb5915f35ac1698
GCollica <49055809+GCollica@users.noreply.github.com>
2020-10-19 22:15:12 +0800

    Reworked Quirk System ready for extension
    As above. Getting ready to merge branch into master so trying to clean up with this push.

---
+++ Assets/Scripts/MacProtoScripts/Quirk.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Quirk", menuName = "Quirk")]
+public class Quirk : ScriptableObject
+{
+    public int quirkID;
+    public string quirkName;
+    public string quirkDescription;
+    public Sprite quirkCardArt;
+    public int quirkRarity;
+}
--- Assets/Scripts/MacProtoScripts/QuirkManager.cs
+++ Assets/Scripts/MacProtoScripts/QuirkManager.cs
@@ -4,19 +4,39 @@ using UnityEngine;
 
 public class QuirkManager : MonoBehaviour
 {
-    public List<GameObject> Quirks;
-
+    public List<Quirk> AllQuirks;
+    public Quirk CurrentQuirk;
+    
+    //public List<GameObject> Quirks;
     //public GameObject totem;
 
     void Start()
     {
-        DamageTotem();
+        SetCurrentQuirk(0);
+    }
+
+    public void SetCurrentQuirk(int quirkIDInput)
+    {
+        if(quirkIDInput == 0)
+        {
+            CurrentQuirk = null;
+        }
+        else
+        {
+            foreach (Quirk quirk in AllQuirks)
+            {
+                if (quirk.quirkID == quirkIDInput)
+                {
+                    CurrentQuirk = quirk;
+                }
+            }
+        }        
     }
 
     #region DamageTotem
     void DamageTotem()
     {
-        Instantiate(Quirks.Find(obj => obj.name == "DamageTotem"), transform);
+        //Instantiate(Quirks.Find(obj => obj.name == "DamageTotem"), transform);
     }
     #endregion
 }
