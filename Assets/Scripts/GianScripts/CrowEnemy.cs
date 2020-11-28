762c15df65119691354d0498825937ef1a323e5b
GCollica <49055809+GCollica@users.noreply.github.com>
2020-11-28 15:41:23 +0800

    started crow work

---
+++ Assets/Scripts/GianScripts/CrowEnemy.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+
+public class CrowEnemy : MonoBehaviour
+{
+    /*Script that will be attached to each basic enemy 1 gameobject throughout the game. Holds individual values for damage, resistance, health & movement speed and feeds that into it's own instance of the BasicEnemyClass. */
+    public BasicEnemyClass basicEnemyClass;
+    private CrowEnemy_AI crowEnemyAI;
+    private QuirkManager quirkManager;
+    private WaveManager waveManager;
+    private Enemy_Animation animationComponent;
+    private EnemyAIPathfinding pathfindingComponent;
+
+    public SpriteRenderer spriteRenderer;
+
+    public float startingDamage = 5f;
+    public float startingResistance = 10f;
+    public float startingHealth = 25f;
+    public float startingMovespeed = 10f;
+    public int goldDrop = 2;
+
+    private GameObject rewardPlayer;
+
+    void Awake()
+    {
+        crowEnemyAI = this.gameObject.GetComponent<CrowEnemy_AI>();
+        animationComponent = this.gameObject.transform.GetComponentInChildren<Enemy_Animation>();
+        pathfindingComponent = this.gameObject.transform.GetComponentInChildren<EnemyAIPathfinding>();
+        
+        if (this.gameObject.transform.GetChild(1) != null)
+        {
+            spriteRenderer = this.gameObject.transform.GetChild(1).GetComponent<SpriteRenderer>();
+        }
+
+        InitialiseClassInstance();
+
+        waveManager = FindObjectOfType<WaveManager>();
+        waveManager.AddActiveEnemy(this.gameObject);
+        quirkManager = FindObjectOfType<QuirkManager>();
+
+        if (quirkManager.CurrentQuirk.quirkID == 2)
+        {
+            int chosenModifier = quirkManager.RandomiseMSModifier();
+            if (chosenModifier == 0)
+            {
+                basicEnemyClass.currentMovementSpeed = (basicEnemyClass.currentMovementSpeed / 2);
+            }
+            if (chosenModifier == 1)
+            {
+                basicEnemyClass.currentMovementSpeed = (basicEnemyClass.currentMovementSpeed * 2);
+            }
+        }
+
+        //SetSortingLayers();
+
+        //InvokeRepeating("SetSortingLayers", 0.5f, 0.5f);
+    }
+
+    //Initialises an instance of the Basic Enemy Class, feeding it values for damage, resistance, health, movespeed as the constructor requires.
+    private void InitialiseClassInstance()
+    {
+        basicEnemyClass = new BasicEnemyClass(startingDamage, startingResistance, startingHealth, startingMovespeed, goldDrop);
+    }
+
+    /*private void SetSortingLayers()
+    {
+        spriteRenderer.sortingLayerName = Mathf.RoundToInt(this.gameObject.transform.position.y).ToString();
+        spriteRenderer.sortingOrder = Mathf.RoundToInt(this.gameObject.transform.position.x) * 10;
+    }*/
+
+    public void TakeDamage(GameObject player, string attackType)
+    {
+
+        if (attackType == "Heavy")
+        {
+            PlayFlinchAnim();
+            basicEnemyClass.TakeCalculatedDamage(player.GetComponent<PlayerStats>().playerClass.currentHeavyDamage);
+            //basicEnemyAI.Knockback();
+
+        }
+        else if (attackType == "Light")
+        {
+            PlayFlinchAnim();
+            basicEnemyClass.TakeCalculatedDamage(player.GetComponent<PlayerStats>().playerClass.currentLightDamage);
+            //basicEnemyAI.Knockback();
+        }
+
+        // If enemy health drops below zero
+        if (basicEnemyClass.currentHealth <= 0)
+        {
+            // Death animation here.
+
+            // Invoke death for animation duration or call it when animation finishes
+            pathfindingComponent.rigidBody.constraints = RigidbodyConstraints2D.FreezeAll;
+            BeginEnemyDeath(player);
+        }
+    }
+
+    public void EnemyDead()
+    {
+        rewardPlayer.GetComponent<PlayerStats>().playerClass.GainGold(basicEnemyClass.currentGoldDrop);
+        waveManager.RemoveActiveEnemy(this.gameObject);
+        Destroy(gameObject);
+    }
+
+    public void BeginEnemyDeath(GameObject rewardPlayerInput)
+    {
+        rewardPlayer = rewardPlayerInput;
+        animationComponent.SetAnimBool("Walking", false);
+        animationComponent.SetAnimBool("Dying", true);
+    }
+
+    public void PlayFlinchAnim()
+    {
+        animationComponent.SetAnimBool("Walking", false);
+        animationComponent.SetAnimBool("Flinching", true);
+    }
+}
---
+++ Assets/Scripts/GianScripts/CrowEnemy_AI.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowEnemy_AI : MonoBehaviour
+{
+    //Data references for state machine components of the code.
+    public enum AIState { Idle, FindingTarget, PursuingTarget, AttachedToTarget };
+    public AIState currentAIState = AIState.Idle;
+
+    //Data references for multi-use components of the code.
+    public GameObject[] totalEnemies;
+    public Transform currentTargetTransform;
+
+    //Date references for idle state of the code.
+    private float idleDelayTimer = 0f;
+    private float idleDelayLength = 1f;
+
+    private float facingDirectionBuffer = 0.125f;
+
+    private float attachDistance = 0.5f;
+
+    private EnemyAIPathfinding pathfindingComponent;
+    private Enemy_Animation animationComponent;
+    public CrowEnemy crowEnemyScript;
+    public QuirkManager quirkManager;
+
+    void Awake()
+    {
+        pathfindingComponent = this.gameObject.GetComponent<EnemyAIPathfinding>();
+        crowEnemyScript = this.gameObject.GetComponent<CrowEnemy>();
+        animationComponent = this.gameObject.transform.GetComponentInChildren<Enemy_Animation>();
+        quirkManager = FindObjectOfType<QuirkManager>();
+    }
+
+    void Update()
+    {
+        switch (currentAIState)
+        {
+            case AIState.Idle:
+
+                pathfindingComponent.ClearPath();
+                animationComponent.SetAnimBool("Walking", false);
+
+                if (idleDelayTimer < idleDelayLength)
+                {
+                    idleDelayTimer += Time.deltaTime;
+                }
+                else if (idleDelayTimer >= idleDelayLength)
+                {
+                    currentAIState = AIState.FindingTarget;
+                }
+                break;
+
+            case AIState.FindingTarget:
+
+                idleDelayTimer = 0f;
+                InitialiseTargets();
+                FindNearestTarget();
+                animationComponent.SetAnimBool("Walking", false);
+                currentAIState = AIState.PursuingTarget;
+                break;
+
+            case AIState.PursuingTarget:
+
+                SetFacingDirection();
+                pathfindingComponent.PursureTarget();
+                animationComponent.SetAnimBool("Walking", true);
+                if (Vector2.Distance(this.transform.position, currentTargetTransform.position) <= attachDistance)
+                {
+                    currentAIState = AIState.AttachedToTarget;
+                }
+                break;
+
+            case AIState.AttachedToTarget:
+
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    //Initialises totalPlayers array.
+    private void InitialiseTargets()
+    {
+        totalEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+
+    //Finds current closest target to this enemy.
+    private void FindNearestTarget()
+    {
+        if (totalEnemies.Length == 0)
+        {
+            return;
+        }
+
+        float closestDistance = (totalEnemies[0].transform.position - this.gameObject.transform.position).magnitude;
+        GameObject closestPlayer = totalEnemies[0];
+
+        foreach (GameObject player in totalEnemies)
+        {
+            if ((player.transform.position - this.gameObject.transform.position).magnitude < closestDistance)
+            {
+                closestPlayer = player;
+            }
+        }
+
+        currentTargetTransform = closestPlayer.transform;
+    }
+
+    //Sets characters Facing Direction by flipping the sprite.
+    private void SetFacingDirection()
+    {
+        if (pathfindingComponent.rigidBody.velocity.x > (0 + facingDirectionBuffer))
+        {
+            this.gameObject.GetComponentInChildren<SpriteRenderer>().flipX = true;
+        }
+        else if (pathfindingComponent.rigidBody.velocity.x <= (0 - facingDirectionBuffer))
+        {
+            this.gameObject.GetComponentInChildren<SpriteRenderer>().flipX = false;
+        }
+    }
+}
