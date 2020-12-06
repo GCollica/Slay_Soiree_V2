8f01743817025dfbb234ee75f775523311e80d7e
MacVP <48820988+MacVP@users.noreply.github.com>
2020-12-06 15:54:05 +0800

    Audio Manager

---
+++ Assets/Scripts/MacScripts/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+        }
+
+        s.source.Play();
+    }
+
+    // Use | FindObjectOfType<AudioManager>().Play("AUDIOCLIPNAME"); | to play an audio clip
+}
