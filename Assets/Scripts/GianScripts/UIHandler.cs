ad842820d4242fb28087d3f44894a7c7b6083486
GCollica <49055809+GCollica@users.noreply.github.com>
2020-12-17 15:03:30 +0800

    UI Update

--- Assets/Scripts/GianScripts/UIHandler.cs
+++ Assets/Scripts/GianScripts/UIHandler.cs
@@ -10,7 +10,7 @@ public class UIHandler : MonoBehaviour
     /// <summary>
     /// Player Stat fields
     /// </summary>
-    public PlayerStats player1Stats;
+    private PlayerStats player1Stats;
     private PlayerStats player2Stats;
     private PlayerStats player3Stats;
     private PlayerStats player4Stats;
@@ -39,6 +39,11 @@ public class UIHandler : MonoBehaviour
     public TMPro.TextMeshProUGUI P2HealthPotValueText;
     public TMPro.TextMeshProUGUI P3HealthPotValueText;
     public TMPro.TextMeshProUGUI P4HealthPotValueText;
+
+    public GameObject P1PressStart;
+    public GameObject P2PressStart;
+    public GameObject P3PressStart;
+    public GameObject P4PressStart;
 
 
     // Start is called before the first frame update
@@ -101,10 +106,19 @@ public class UIHandler : MonoBehaviour
             case 1:
                 if (player1Stats == null)
                 {
+                    P1HealthBar.fillAmount = 0f;
+                    P1GoldValueText.text = " ";
+                    P1HealthPotValueText.text = " ";
                     break;
                 }
 
-                p1HealthBarValue = player1Stats.playerClass.currentHealth / 100f;
+                if(P1PressStart.activeInHierarchy == true)
+                {
+                    P1PressStart.SetActive(false);
+                }
+                
+
+                p1HealthBarValue = player1Stats.playerClass.currentHealth / player1Stats.playerClass.maxHealth;
                 P1HealthBar.fillAmount = p1HealthBarValue;
 
                 P1GoldValueText.text = "Gold: " + player1Stats.playerClass.currentGold.ToString();
@@ -114,10 +128,18 @@ public class UIHandler : MonoBehaviour
             case 2:
                 if (player2Stats == null)
                 {
+                    P2HealthBar.fillAmount = 0f;
+                    P2GoldValueText.text = " ";
+                    P2HealthPotValueText.text = " ";
                     break;
                 }
 
-                p2HealthBarValue = player2Stats.playerClass.currentHealth / 100f;
+                if (P2PressStart.activeInHierarchy == true)
+                {
+                    P2PressStart.SetActive(false);
+                }
+
+                p2HealthBarValue = player2Stats.playerClass.currentHealth / player2Stats.playerClass.maxHealth;
                 P2HealthBar.fillAmount = p2HealthBarValue;
 
                 P2GoldValueText.text = "Gold: " + player2Stats.playerClass.currentGold.ToString();
@@ -127,10 +149,18 @@ public class UIHandler : MonoBehaviour
             case 3:
                 if (player3Stats == null)
                 {
+                    P3HealthBar.fillAmount = 0f;
+                    P3GoldValueText.text = " ";
+                    P3HealthPotValueText.text = " ";
                     break;
                 }
 
-                p3HealthBarValue = player3Stats.playerClass.currentHealth / 100f;
+                if (P3PressStart.activeInHierarchy == true)
+                {
+                    P3PressStart.SetActive(false);
+                }
+
+                p3HealthBarValue = player3Stats.playerClass.currentHealth / player3Stats.playerClass.maxHealth;
                 P3HealthBar.fillAmount = p3HealthBarValue;
 
                 P3GoldValueText.text = "Gold: " + player3Stats.playerClass.currentGold.ToString();
@@ -139,10 +169,18 @@ public class UIHandler : MonoBehaviour
             case 4:
                 if (player4Stats == null)
                 {
+                    P4HealthBar.fillAmount = 0f;
+                    P4GoldValueText.text = " ";
+                    P4HealthPotValueText.text = " ";
                     break;
                 }
 
-                p4HealthBarValue = player4Stats.playerClass.currentHealth / 100f;
+                if (P4PressStart.activeInHierarchy == true)
+                {
+                    P4PressStart.SetActive(false);
+                }
+
+                p4HealthBarValue = player4Stats.playerClass.currentHealth / player4Stats.playerClass.maxHealth;
                 P4HealthBar.fillAmount = p4HealthBarValue;
 
                 P4GoldValueText.text = "Gold: " + player4Stats.playerClass.currentGold.ToString();
