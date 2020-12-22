6d3a1f9e3c4af44a28e358912ab689b2c41e7d4f
GCollica <49055809+GCollica@users.noreply.github.com>
2020-12-22 12:17:23 +0800

    Added Interact Indicator, repositioned and rescaled quirk pick collider

---
+++ Assets/InteractIndicator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractIndicator : MonoBehaviour
+{
+    public GameObject InteractIndicatorGO;
+
+    private void ToggleActive()
+    {
+        if(InteractIndicatorGO.activeInHierarchy == true)
+        {
+            InteractIndicatorGO.SetActive(false);
+        }
+        else
+        {
+            InteractIndicatorGO.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("ExitDoor") || collision.gameObject.CompareTag("ItemPedistool") || collision.gameObject.CompareTag("QuirkCard"))
+        {
+            ToggleActive();
+        }      
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("ExitDoor") || collision.gameObject.CompareTag("ItemPedistool") || collision.gameObject.CompareTag("QuirkCard"))
+        {
+            ToggleActive();
+        }
+    }
+}
